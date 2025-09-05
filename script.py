ef8f050f4a329c03d206e3e821dae4bca9a9c2ea
clbokea <clbo@kea.dk>
2025-09-05 10:10:26 +0200

    3. parts

--- script.py
+++ script.py
@@ -15,6 +15,48 @@
 
 
 
+
+
+
+
+
+
+
+"""
+import subprocess as sp
+
+sp.run(['git', 'pull', 'origin', 'master' ])
+"""
+## jeg vil gerne læse filer fra mit filsystem
+"""
+import os
+
+#print( os.mkdir('TEST')    )
+os.chdir('TEST')
+print( os.getcwd()     )
+f = open('testfil.txt', 'w')
+os.chdir('..')
+print( os.getcwd()     )
+
+os.chdir('TEST')
+os.remove('testfil.txt')
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 """
 # import data
 from data import name as xxx
